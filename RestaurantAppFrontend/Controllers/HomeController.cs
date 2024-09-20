082a751acb84ae5674f2073c4eb8e58df4c9672a
Hjalmar Stranninge <Hjalmar.stranninge@outlook.com>
2024-09-20 14:11:02 +0200

    Added HttpClient

--- RestaurantAppFrontend/Controllers/HomeController.cs
+++ RestaurantAppFrontend/Controllers/HomeController.cs
@@ -1,26 +1,37 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Razor.TagHelpers;
 using RestaurantAppFrontend.Models;
 using System.Diagnostics;
+
 
 namespace RestaurantAppFrontend.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly HttpClient _httpClient;
+        private string _baseUri = "https://localhost:7185/";
 
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, HttpClient httpClient)
         {
             _logger = logger;
+            _httpClient = httpClient;
         }
 
-        public IActionResult Index()
+
+        public async Task<IActionResult> Index()
         {
+            // Fetching a long string from a txt file and putting it in a ViewData
             string filePath = Path.Combine(Directory.GetCurrentDirectory(), "C:\\Users\\Hjalm\\source\\repos\\RestaurantAppFrontend\\RestaurantAppFrontend\\wwwroot\\Resources\\info.txt");
-
             string fileContent = System.IO.File.ReadAllText(filePath);
-
             ViewData["FileContent"] = fileContent;
+
+            var response = await _httpClient.GetAsync($"{_baseUri}/getallmenuitems");  
+
+            var json = await response.Content.ReadAsStringAsync();
+
+
             return View();
         }
 
