f989dece0b728765d99282819e0122ab2a7d9e8f
Hjalmar Stranninge <Hjalmar.stranninge@outlook.com>
2024-09-23 12:47:53 +0200

    Added menu content and styling

--- RestaurantAppFrontend/Models/MenuItem.cs
+++ RestaurantAppFrontend/Models/MenuItem.cs
@@ -6,6 +6,11 @@
         public string Name { get; set; }
         public double Price { get; set; }
         public bool IsAvailable { get; set; }
-        public string? ImageUrl { get; set; }   
+        public string? ImageUrl { get; set; }
+        public string Description { get; set; }
+        public bool IsAppetizer { get; set; }
+        public bool IsMainCourse { get; set; }
+        public bool IsDesert { get; set; }
+        public bool IsCocktail { get; set; }
     }
 }
